0cba851cac8eb197e3534ea5b6d34440faab76d3
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-10 12:56:44 +0300

    Выполнено

--- task_2_3.py
+++ task_2_3.py
@@ -16,3 +16,7 @@ def switch_it_up(number):
         return sw [number]
 print(switch_it_up(number))
 
+# если не через списки, то вот как еще можно интересно сделать)
+def switch_it_up(number):
+    return {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five',
+            6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine'}.get(number)
